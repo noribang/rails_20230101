fec48e00bf2c234fb8d8d2b72c5f6c783b1e8128
hoonkim <hoonk9387@gmail.com>
2023-01-01 19:44:56 -0600

    Added index and show methods in controller.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,22 @@
 class BirdsController < ApplicationController
+
+    # GET /birds
+    def index
+        # byebug
+        birds = Bird.all
+        render json: birds
+    end
+
+    # GET /birds/:id
+    def show
+        bird = Bird.find_by(id: params[:id])
+        # render json: bird
+        if bird
+            render json: bird
+        else
+            render json: { error: "Bird not found" }, status: :not_found 
+        end
+
+    end
+
 end
