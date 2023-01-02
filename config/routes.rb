050ff27c05f64971b6821295c372b654d4b03951
hoonkim <hoonk9387@gmail.com>
2023-01-01 19:27:19 -0600

    Added routes mapped to show and index actions using resources method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # GET request all
-  get "/birds", to: "birds#index"
-  # GET request by :id
-  get "/birds/:id", to: "birds#show"
+  # # GET request all
+  # get "/birds", to: "birds#index"
+  # # GET request by :id
+  # get "/birds/:id", to: "birds#show"
+
+  # # Generates all routes. 
+  # resources :birds
+  # Generate using only:
+  resources :birds, only: [:index, :show]
 
 end
