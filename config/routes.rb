b7d1dea2672cb1890f308809481854e1cec95714
hoonkim <hoonk9387@gmail.com>
2023-01-01 19:11:56 -0600

    Added routes /birds and /birds/:id. Mapped routes to birds#index and birds#show.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # GET request all
+  get "/birds", to: "birds#index"
+  # GET request by :id
+  get "/birds/:id", to: "birds#show"
+
 end
