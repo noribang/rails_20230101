b07f58d6e26a5a2d3a415a6c95aa0ee2c5a76fa5
hoonkim <hoonk9387@gmail.com>
2023-01-04 21:12:18 -0600

    Added update action in controller. Updated strong params method to include :likes.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -30,10 +30,22 @@ class BirdsController < ApplicationController
         render json: bird, status: :created
     end 
 
+    # PATCH /birds/:id
+    def update
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            bird.update(bird_params)
+            render json: bird
+        else
+            render json: { error: "Bird not found."}, status: :not_found
+        end
+    end
+
+
     private
 
     def bird_params
-        params.permit(:name, :species)
+        params.permit(:name, :species, :likes)
     end
 
 end
