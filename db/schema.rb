dff47974542e41ebb4820b30021bcb3703451e16
hoonkim <hoonk9387@gmail.com>
2023-01-04 21:00:19 -0600

    Created new migration to add likes column to birds table. Ran migration. Reset db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_02_005411) do
+ActiveRecord::Schema.define(version: 2023_01_05_025350) do
 
   create_table "birds", force: :cascade do |t|
     t.string "name"
     t.string "species"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes"
   end
 
 end
